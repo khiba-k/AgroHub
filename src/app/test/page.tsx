726b714dd9a30078c4d8db4e5838b33e5007d10b
Mampheteng <mamphetengkoote402@gmail.com>
2025-07-08 15:23:31 +0200

    farmerprofile form

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import FarmerProfile from '@/screens/profile/farmer/FarmerProfile'
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <div>
+   <FarmerProfile/>   
+    </div>
+  )
+}
+
+export default page
